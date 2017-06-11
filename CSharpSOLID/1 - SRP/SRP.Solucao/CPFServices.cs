55adf1d077174eff0549cb4dfe38ffed8cfb232e
Carlos H. M <carlosmarian@gmail.com>
2017-06-11 10:27:44 -0300

    SOLID SRP

---
+++ CSharpSOLID/1 - SRP/SRP.Solucao/CPFServices.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CSharpSOLID._1___SRP.SRP.Solucao
+{
+    public static class CPFServices
+    {
+        public static bool IsValid(string cpf)
+        {
+            return cpf.Length == 11;
+        }
+    }
+}
---
+++ CSharpSOLID/1 - SRP/SRP.Solucao/UsuarioService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpSOLID._1___SRP.SRP.Solucao
+{
+    public class UsuarioService
+    {
+        public string AdicionarUsuario(Usuario usuario)
+        {
+            if (!usuario.IsValid())
+                return "Dados inválidos";
+
+            var repo = new UsuarioRepository();
+            repo.AdicionarUsuario(usuario);
+
+            EmailServices.Enviar("csharp@solid.com", usuario.Email, "Bem Vindo", "Parabéns está Cadastrado");
+
+            return "Usuário cadastrado com sucesso";
+        }
+    }
+}
