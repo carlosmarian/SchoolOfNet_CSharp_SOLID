832de0e1ae13af99ff53be8b93b0f24027dd67e2
Carlos H. M <carlosmarian@gmail.com>
2017-06-15 16:05:06 -0300

    Ajuste na interface

--- CSharpSOLID/4 - ISP/ISP.Solucao/Interfaces/ICadastroUsuario.cs
+++ CSharpSOLID/4 - ISP/ISP.Solucao/Interfaces/ICadastroUsuario.cs
@@ -1,10 +1,10 @@
 ﻿//ISP = Princípio da Segregação de Interface
 namespace CSharpOO.SOLID.ISP.Solucao.Interfaces
 {
-    public interface ICadastroUsuario
+    public interface ICadastroUsuario : ICadastro
     {
-        void ValidarDados();
-        void Salvar();
+       // void ValidarDados();
+       // void Salvar();
         void EnviarEmail();
     }
 }
